2251124e47585cda8f88dc1156dffdc2d8256610
bittern <bittern@EMAIL.com>
2017-08-01 19:10:43 +0530

    change

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "4395d253ebf82756161db890ece58c75" 
   config.secret = "42bced3ba4c338dd6dbb15c3077c1595"
-  config.redirect_ui "http://jioapp12.herokuapp.com/auth/shopify/callback"
+  # config.redirect_ui "http://jioapp12.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
