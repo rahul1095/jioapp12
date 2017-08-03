df22e6dbe9fb36d9aeb87310c504671dd00aded5
bittern <bittern@EMAIL.com>
2017-08-03 15:49:49 +0530

    change sopify.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = ENV['SOPIFY_API_KEY'] 
-  config.secret = ENV['SOPIFY_SECRET']
+  config.api_key = "4395d253ebf82756161db890ece58c75" 
+  config.secret = "42bced3ba4c338dd6dbb15c3077c1595"
   # config.redirect_ui "http://jioapp12.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
