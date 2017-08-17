62ac9266f8fc73cb013f8345f6d1d00d070ffb02
bittern <bittern@EMAIL.com>
2017-08-17 19:34:41 +0530

    json 200 request

--- app/controllers/app_proxy_controller.rb
+++ app/controllers/app_proxy_controller.rb
@@ -7,10 +7,12 @@ class AppProxyController < ApplicationController
 
   def product_create
    Home.create(gather: params)
+   # respond_with(@products.to_json(:include => [:title, :variants.first.compare_at_price, :body_html]), :location => home_path)
+   render json:{ status:200}
   end   
 
   def product_update
-     puts "hello i am rahul" 
+    
   end
 
   def collection_create
